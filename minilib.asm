fac50ed00ccc58cbfbd002374954f69f267746cd
puilla_e <edouard.puillandre@epitech.eu>
2017-03-06 22:51:57 +0100

    correct test and cppy strchr

--- minilib.asm
+++ minilib.asm
@@ -22,16 +22,16 @@ strcmp_loop:
 	je  end_strcmp_loop
 	cmp byte [rsi+rcx],0
 	je  end_strcmp_loop
-	xor rdx,rdx
-	mov dl,[rdi+rcx]
+	xor rdx,rdx		
+	mov rdx,[rdi+rcx]
 	cmp byte [rsi+rcx],dl
 	jne  end_strcmp_loop
 	inc rcx
 	jmp strcmp_loop
 	
 end_strcmp_loop:
-	xor rdx,rdx
-	mov dl,[rdi+rcx]
+	xor rdx,rdx		
+	mov rdx,[rdi+rcx]
 	cmp byte[rsi+rcx],dl
 	jge end_strcmp_neg
 	xor rax, rax	
@@ -45,3 +45,18 @@ end_strcmp_neg:
 	sub al, [rdi+rcx]
 	neg rax
 	ret
+
+strchr:
+					;rdi is the base recuperation arguments
+	mov rcx, 0 			;taking a couter (rcx can be an argument) and setting it to zero
+	
+while_strchr: cmp BYTE[rdi + rcx], sil 		;comparison between the byte of rdi + rcx (sil is the fist 8 bits of the parameter)
+	je end_strchr
+	cmp byte[rdi + rcx], 0			;comparison between the byte and the \0
+  	je end_strchr					;if 0 go to end
+	inc rcx				  	  ;incrementing counter for next char
+	jmp while_strchr	  			  ;go back to while
+	
+end_strchr:
+	mov rax, rdi 				;setting the return value to rdi
+	ret	 				 ;return rax wich is the value for return
--- test_func.c
+++ test_func.c
@@ -2,11 +2,15 @@
 
 size_t strlen(char *str);
 int strcmp(const char *s1, const char *s2);
+char *strchr(const char *s, int c);
 
 int main(int ac, char **av)
 {
   if (ac >= 2)
-    printf("%s got len of %lu\n",av[1], strlen(av[1]));
+    printf("STRLEN: %s got len of %lu\n",av[1], strlen(av[1]));
   if (ac >= 3)
-    printf("cmp %s and %s = %d\n", av[1], av[2], strcmp(av[1], av[2]));
+    printf("STRCMP: cmp %s and %s = %d\n", av[1], av[2], strcmp(av[1], av[2]));
+  if (ac >= 3)
+    printf("STRCHR: %s and %c = %s\n",
+	   av[1], av[2][0], strchr(av[1], av[2][0]));
 }
