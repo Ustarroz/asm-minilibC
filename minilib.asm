f35468ef19f0c13e2ad0e63d67d044f554b238d8
puilla_e <edouard.puillandre@epitech.eu>
2017-03-07 10:26:39 +0100

    add strcasecmp + start strncmp + improve test

--- minilib.asm
+++ minilib.asm
@@ -1,7 +1,11 @@
 global strlen:function
-global strcmp:function
+global strlen:function	
+global strncmp:function
+global strcasecmp:function
+global strchr:function	
 section .text
-	
+
+	;; strlen
 strlen:
 	xor rax,rax
 
@@ -14,6 +18,8 @@ strlen_loop:
 end_strlen_loop:
 	ret
 
+	;; strcmp
+	
 strcmp:
 	xor rcx,rcx
 	
@@ -46,6 +52,7 @@ end_strcmp_neg:
 	neg rax
 	ret
 
+	;; strchr
 strchr:
 					;rdi is the base recuperation arguments
 	mov rcx, 0 			;taking a couter (rcx can be an argument) and setting it to zero
@@ -60,3 +67,103 @@ while_strchr: cmp BYTE[rdi + rcx], sil 		;comparison between the byte of rdi + r
 end_strchr:
 	mov rax, rdi 				;setting the return value to rdi
 	ret	 				 ;return rax wich is the value for return
+
+	;; strncmp
+strncmp:
+	xor r8,r8
+	xor rax, rax
+	cmp rdx,0
+	je end_strncmp
+	
+strncmp_loop:
+	cmp r8,rdx
+	je  end_strncmp_loop	
+	cmp byte [rdi+r8],0
+	je  end_strncmp_loop
+	cmp byte [rsi+r8],0
+	je  end_strncmp_loop
+	xor rcx,rcx		
+	mov rcx,[rdi+r8]
+	cmp byte [rsi+r8],cl
+	jne  end_strncmp_loop
+	inc r8
+	jmp strncmp_loop
+	
+end_strncmp_loop:
+	xor rcx,rcx		
+	mov rcx,[rdi+r8]
+	cmp byte[rsi+r8],cl
+	jge end_strncmp_neg
+	mov al, [rdi+r8]
+	sub al, [rsi+r8]
+	ret
+
+end_strncmp_neg:
+	mov al, [rsi+rcx]
+	sub al, [rdi+rcx]
+	neg rax
+end_strncmp:
+	ret
+
+	;; strcasecmp
+strcasecmp:
+	xor r8,r8
+	xor rdx,rdx
+	mov rdx,rdi
+	xor rcx,rcx
+	mov rcx,rsi
+	
+strcasecmp_loop:
+	cmp byte [rdx+r8],0
+	je  end_strcasecmp_loop
+	cmp byte [rcx+r8],0
+	je  end_strcasecmp_loop
+	xor rdi,rdi
+	mov dil,[rdx+r8]
+	xor rsi,rsi
+	xor sil,[rcx+r8]
+	call strcasecmp_char
+	cmp rax,0
+	jne end_strcasecmp_loop
+	inc r8
+	jmp strcasecmp_loop
+	
+end_strcasecmp_loop:
+	xor rdi,rdi
+	mov dil,[rdx+r8]
+	xor rsi,rsi
+	xor sil,[rcx+r8]
+	call strcasecmp_char
+	ret
+
+strcasecmp_char:
+	call case_char
+	xor rdi,rdi
+	mov rdi, rsi
+	xor rsi,rsi
+	mov rsi, rax
+	call case_char
+	xor rdi,rdi
+	mov rdi, rax
+	xor rax,rax
+	cmp rsi, rdi
+	jg strcasecmp_char_neg
+	mov rax,rsi
+	sub rax,rdi
+	ret
+strcasecmp_char_neg:
+	mov rax,rdi
+	sub rax,rsi
+	neg rax
+	ret
+	
+case_char:
+	xor rax,rax
+	mov rax,rdi
+	cmp al,65
+	jl end_case_char
+	cmp al,90
+	jg end_case_char
+	add rax,32
+end_case_char:
+	ret
--- test_func.c
+++ test_func.c
@@ -2,6 +2,8 @@
 
 size_t strlen(char *str);
 int strcmp(const char *s1, const char *s2);
+int strcasecmp(const char *s1, const char *s2);
+int strncmp(const char *s1, const char *s2, size_t n);
 char *strchr(const char *s, int c);
 
 int main(int ac, char **av)
@@ -13,4 +15,16 @@ int main(int ac, char **av)
   if (ac >= 3)
     printf("STRCHR: %s and %c = %s\n",
 	   av[1], av[2][0], strchr(av[1], av[2][0]));
+  if (ac >= 3)
+    {
+      //printf("STRNCMP: cmp %s and %s for %d bytes = %d\n",
+      //av[1], av[2], 3, strncmp(av[1], av[2], 3));
+      printf("STRNCMP: cmp %s and %s for %d bytes = %d\n",
+      	   av[1], av[2], 0, strncmp(av[1], av[2], 0));
+    }
+  printf("STRCASECMP:\n");
+  printf("compare abc, abc: %d\n", strcasecmp("abc", "abc"));
+  printf("compare abc, ABC: %d\n", strcasecmp("abc", "ABC"));
+  printf("compare AB0, abC: %d\n", strcasecmp("AB0", "abC"));
+  printf("compare AB, abC: %d\n", strcasecmp("AB", "abC"));
 }
